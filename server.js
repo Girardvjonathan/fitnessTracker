e5f8370e2c192ce29aaba67b1fdca22e5dfb0222
Jonathan Girard Viau <girardvjonathan@gmail.com>
2017-01-18 19:36:01 -0500

    SSL

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
-
 app.use(express.static(__dirname + '/src/'));
 
-app.listen(process.env.PORT || 8002);
+var fs = require('fs');
+var https = require('https');
+var app = require('express')();
+var options = {
+   key  : fs.readFileSync('server.key'),
+   cert : fs.readFileSync('server.crt')
+};
+
+app.listen(process.env.PORT || 80);
